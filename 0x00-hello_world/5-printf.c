6a9c7dd6f39954868087cad66755653efc5e7351
fentyjames <fentyjconteh@yahoo.com>
2022-09-11 03:03:23 -0700

    5 Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stido.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcom is piece of art,\n");
+	return (0);
+}
