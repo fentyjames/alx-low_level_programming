7d03cb3586930879d8fb84cb4b4d661734092d1f
fentyjames <fentyjconteh@yahoo.com>
2022-09-12 13:53:30 -0700

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- *  main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  * 
- *  Return: Always 1 (Success)
+ * 
+ * program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Return: 1 if success.
  */
-
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
+
