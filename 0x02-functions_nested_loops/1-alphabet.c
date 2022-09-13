d4defa12d6789e80c53b972b4cf7c434a791192b
fentyjames <fentyjconteh@yahoo.com>
2022-09-13 03:18:45 -0700

    #1 Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+
+}
