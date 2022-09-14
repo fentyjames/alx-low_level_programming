09f206b4393ccfffb32f4afea4d25748eb6ed98a
fentyjames <fentyjconteh@yahoo.com>
2022-09-14 14:14:22 -0700

    #2 prints 10 time alphabet lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,8 @@
 
 /**
  * print_alphabet_x10 -  prints the alphabet x10
- *  function that prints 10 times the alphabet, in lowercase, followed by a new line.
+ * function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
  */
 
 void print_alphabet_x10(void)
