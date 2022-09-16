ffd4948e2e69101b7d0ebc2885f98a10e95eb278
fentyjames <fentyjconteh@yahoo.com>
2022-09-16 23:44:55 +0300

    Task #6 Function to print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_line - prints a straight line
+*@n: parameter
+*Return:returns nothing
+*/
+
+void print_line(int n)
+{
+	int c;
+
+	while (n >= 0 & n <= c)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
