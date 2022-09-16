6788689e4a54f1d48cc08d5289787e03a6a417a9
fentyjames <fentyjconteh@yahoo.com>
2022-09-17 00:39:54 +0300

    Task #9 Function to print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,27 +5,26 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
+	for (i = 1; i < 100; i++)
+		if (i % 15 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else if (i % 5 == 0)
 		{
-			if (i == 100)
-			{
-				printf("Buzz");
-				printf("\n");
-			}
-			else
-				printf("Buzz ");
+			printf("Buzz ");
 		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz ");
+		}
 		else
+		{
 			printf("%d ", i);
-	i++;
-	}
-return (0);
+		}
+	printf("Buzz");
+	printf("\n");
+	return (0);
 }
